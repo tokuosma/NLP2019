76062b3c2372511a222926320553cb21c6cc7e9a
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2020-01-03 22:56:37 +0200

    Work on notebook and report

--- util.py
+++ util.py
@@ -1,7 +1,10 @@
 import json
 import re
+from datetime import datetime
 import argparse
 import os.path
+
+CREATED_AT_DATE_FORMAT = "%a %b %d %X %z %Y"
 
 def read_tweets(paths):
     """ Returns a list of tweet objects obtained from reading all tweet json files specified in paths
@@ -36,6 +39,23 @@ def clean_tweet_text(text):
     text = re.sub(r'https?:\/\/.*[a-zA-Z0-9.\/_%…]*', '', text, flags=re.MULTILINE)
     text = re.sub(r'@[a-zA-Z0-9.\/_%…]*', '', text, flags=re.MULTILINE)
     return text
+
+def print_hashtag_summary(hashtag, tweets):
+    """ Prints summary of the labeled tweet data for a given hash tag
+        Keyword arguments:
+        hashtag -- Tweet hashtag
+        tweets -- Tweets related to the hash tag
+    """
+    hate_tweets = [tweet for tweet in tweets if tweet['hate_speech'] == True]
+    non_hate_tweets = [tweet for tweet in tweets if tweet['hate_speech'] == False]
+    print('#' + hashtag + 
+        ': Number of hate speech = ' + str(len(hate_tweets)) + 
+        ', Number of non hate speech = ' + str(len(non_hate_tweets)) +
+        ', Total number of tweets = ' +str(len(tweets)))
+    tweet_dates = [datetime.strptime(tweet['created_at'], CREATED_AT_DATE_FORMAT).date() for tweet in tweets]
+    print('Oldest tweet date: ' + str(min(tweet_dates)))
+    print('Newest tweet date: ' + str(max(tweet_dates)))
+    print('\n')    
 
 if __name__ == "__main__":
     pass
